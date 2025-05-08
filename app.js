ff48277dc3524bedfa8e5f70ab95eedd66e98de0
fayedsazli <fayedsazli07@gmail.com>
2025-05-08 10:04:54 +0800

    feat : setup database conection and endpoint

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(express.json());
+
+// Database Connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// Cek Koneksi Database
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed: ", err.message);
+    return;
+  }
+  console.log("🔥 Connected to MySQL Database!");
+});
+
+// Test Endpoint
+app.get("/", (req, res) => {
+  res.send("EduCourse App is Running!");
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
